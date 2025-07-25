f70eb8b1c897429736fc57a9dc3b3d1ddb444d58
Ashminita_91 <ashminitabaliarsingh2004@gmail.com>
2025-07-26 00:03:54 +0530

    Initial Terraform CodePipeline setup

---
+++ main.tf
@@ -0,0 +1,203 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+##############################
+# S3 Bucket for Artifacts
+##############################
+resource "aws_s3_bucket" "codepipeline_bucket" {
+  bucket = "my-pipeline-artifacts-bucket-1234"
+  force_destroy = true
+}
+
+##############################
+# IAM Roles and Policies
+##############################
+resource "aws_iam_role" "codepipeline_role" {
+  name = "codepipeline-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "codepipeline.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Inline policy for CodePipeline role
+resource "aws_iam_role_policy" "codepipeline_policy" {
+  name = "codepipeline-inline-policy"
+  role = aws_iam_role.codepipeline_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "s3:*",
+          "codebuild:*",
+          "codedeploy:*",
+          "iam:PassRole",
+          "cloudwatch:*",
+          "logs:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+
+resource "aws_iam_role" "codebuild_role" {
+  name = "codebuild-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "codebuild.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+resource "aws_iam_role" "codedeploy_role" {
+  name = "codedeploy-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "codedeploy.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_policy" {
+  role       = aws_iam_role.codedeploy_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
+}
+
+##############################
+# CodeBuild Project
+##############################
+resource "aws_codebuild_project" "build_project" {
+  name          = "my-build-project"
+  service_role  = aws_iam_role.codebuild_role.arn
+  artifacts {
+    type = "CODEPIPELINE"
+  }
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:5.0"
+    type                        = "LINUX_CONTAINER"
+    privileged_mode             = true
+  }
+  source {
+    type      = "CODEPIPELINE"
+    buildspec = "buildspec.yml"
+  }
+}
+
+##############################
+# CodeDeploy Application and Group
+##############################
+resource "aws_codedeploy_app" "app" {
+  name = "my-codedeploy-app"
+  compute_platform = "Server"
+}
+
+resource "aws_codedeploy_deployment_group" "group" {
+  app_name              = aws_codedeploy_app.app.name
+  deployment_group_name = "my-deployment-group"
+  service_role_arn      = aws_iam_role.codedeploy_role.arn
+
+  deployment_config_name = "CodeDeployDefault.OneAtATime"
+
+  ec2_tag_set {
+    ec2_tag_filter {
+      key   = "Name"
+      type  = "KEY_AND_VALUE"
+      value = "MyEC2AppServer"
+    }
+  }
+}
+
+##############################
+# CodeStar Connection
+##############################
+resource "aws_codestarconnections_connection" "github_connection" {
+  name          = "my-github-connection"
+  provider_type = "GitHub"
+}
+
+##############################
+# CodePipeline
+##############################
+resource "aws_codepipeline" "pipeline" {
+  name     = "my-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+  artifact_store {
+    location = aws_s3_bucket.codepipeline_bucket.bucket
+    type     = "S3"
+  }
+
+  stage {
+    name = "Source"
+    action {
+      name             = "Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        ConnectionArn    = aws_codestarconnections_connection.github_connection.arn
+        FullRepositoryId = "Ashminita/DevOps"
+        BranchName       = "main"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+    action {
+      name             = "Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.build_project.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+    action {
+      name            = "Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "CodeDeploy"
+      input_artifacts = ["build_output"]
+      version         = "1"
+
+      configuration = {
+        ApplicationName     = aws_codedeploy_app.app.name
+        DeploymentGroupName = aws_codedeploy_deployment_group.group.deployment_group_name
+      }
+    }
+  }
+}
